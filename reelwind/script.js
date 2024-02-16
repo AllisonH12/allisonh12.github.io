0636c62b6c80978e5e18ab03b470edf96c06d763
Allison Huang <111381124+AllisonH12@users.noreply.github.com>
2024-02-16 03:05:04 +0000

    realwind first checkin

---
+++ reelwind/script.js
@@ -0,0 +1,7 @@
+document.getElementById('join-us').addEventListener('click', function() {
+    window.location.href = './signup.html';
+});
+
+document.getElementById('share-story').addEventListener('click', function() {
+    window.location.href = './share.html';
+});
